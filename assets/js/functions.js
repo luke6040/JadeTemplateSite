ef324e2b8cce44b315b265eab569d90ee5fcfc03
Luke Lumley <luke6040@hotmail.com>
2016-03-09 15:40:25 +0000

    mentoring
    Responsive

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,11 +7,22 @@ function mentoringBubbleClick() {
     var $this = $(this),
         faceTop = $this.position().top,
         vertMath = -1 * (faceTop - 230);
+        faceLeft = $this.position().left,
+        horizMath = 0 - faceLeft;
 
-    $this.parent().css('top', + vertMath +'px')
-
-    $this.addClass('has-bubble-open')
-      .siblings().removeClass('has-bubble-open');
+    if($(window).width() > 640) {
+      $this.parent().css('top', + vertMath +'px')
+    } else {
+      if($this.hasClass('back-btn')){
+        mentoringNarrowStart();
+      } else {
+        $this.parent().css('left', + horizMath +'px')
+      }
+    }
+    if(!$this.hasClass('back-btn')){
+      $this.addClass('has-bubble-open')
+        .siblings().removeClass('has-bubble-open');
+    }
   });
 }
 
@@ -31,13 +42,42 @@ function startMentoring() {
 
   var wScroll = $(window).scrollTop();
 
-  if($('section.mentoring').offset().top -500 < wScroll){
+  if($('section.mentoring').offset().top - 500 < wScroll){
+    if($(window).width() > 640){
     $('.faces').addClass('launched');
-    setTimeout(function(){
-      $('.face:nth-child(3)').addClass('has-bubble-open');
-    }, 400);
-
+      if(!$('.face').hasClass('has-bubble-open') && !$this.hasclass('back-btn')){
+        setTimeout(function(){
+          $('.face:nth-child(3)').addClass('has-bubble-open');
+        }, 400);
+      }
+    } else {
+      mentoringNarrowStart();
+    }
   }
-
-
 };
+
+function mentoringNarrowStart() {
+  $('.faces').css({
+    'top': '230px',
+    'left': '0px'
+  });
+  $('.face').first().addClass('has-bubble-open')
+    .siblings().removeClass('has-bubble-open');
+}
+
+function mentoringWideStart() {
+  $('.faces').css({
+    'top': '0px',
+    'left': '0px'
+  });
+  $('.face:nth-child(3)').addClass('has-bubble-open')
+    .siblings().removeClass('has-bubble-open');
+}
+
+$(window).resize(function(){
+  if($(window).width() > 640){
+    mentoringWideStart();
+  }else {
+    mentoringNarrowStart();
+  }
+});
